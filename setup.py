47aa1d99782323e2ac06711e345c2e732ad2bf61
Brian Schubert <brianm.schubert@gmail.com>
2019-09-23 12:43:39 -0400

    Add initial setup script

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+from setuptools import setup, find_packages
+
+BASE_DIR = os.path.dirname(__file__)
+
+with open(os.path.join(BASE_DIR, 'README.rst')) as readme:
+    README = readme.read()
+
+setup(
+    name='Slack2Doc Scribe',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description="A simple Flask app to log Slack messages to a Google Docs "
+                "document for permanent and accessible storage.",
+    long_description=README,
+    zip_safe=False,
+    install_requires=['Flask']
+)
